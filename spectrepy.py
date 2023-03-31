eade8ebc8d175f4ad5e1ab57afcd55592dc80bad
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2023-03-30 22:27:37 -0400

    Add initial python module scaffolding and repo automation

---
+++ spectrepy.py
@@ -0,0 +1,8 @@
+"""Python library bindings for the Spectre password manager"""
+
+__title__ = "spectrepy"
+__summary__ = "Python bindings for the Spectre Password Manager"
+__version__ = "0.1.0"
+__url__ = "https://github.com/enpaul/spectrepy/"
+__license__ = "MIT"
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
